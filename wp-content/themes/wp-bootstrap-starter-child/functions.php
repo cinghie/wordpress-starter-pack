f7bda81ebe43e80f64874322c913d296b0244940
Giando <giando.olini@gogodigital.it>
2018-10-14 16:41:23 +0200

    Hide Wordpress version

--- wp-content/themes/wp-bootstrap-starter-child/functions.php
+++ wp-content/themes/wp-bootstrap-starter-child/functions.php
@@ -2,8 +2,14 @@
 
 require_once 'aq_resizer.php';
 
-add_action( 'wp_enqueue_scripts', 'wp_bootstrap_starter_child_enqueue_styles' );
+function hide_wordpress_version() {
+	return '';
+}
+
+add_filter('the_generator', 'hide_wordpress_version');
 
 function wp_bootstrap_starter_child_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+add_action( 'wp_enqueue_scripts', 'wp_bootstrap_starter_child_enqueue_styles' );
