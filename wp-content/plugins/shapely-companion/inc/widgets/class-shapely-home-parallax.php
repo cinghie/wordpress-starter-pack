63f3e06b977f7827f9697c789c399eaffceee3bc
Giando <giando.olini@gogodigital.it>
2018-12-22 09:31:42 +0100

    Update db

--- wp-content/plugins/shapely-companion/inc/widgets/class-shapely-home-parallax.php
+++ wp-content/plugins/shapely-companion/inc/widgets/class-shapely-home-parallax.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Homepage parralax section Widget
+ * Homepage parallax section Widget
  * Shapely Theme
  */
 class Shapely_Home_Parallax extends WP_Widget {
@@ -18,7 +18,7 @@ class Shapely_Home_Parallax extends WP_Widget {
 			'description'                 => esc_html__( 'Shapely FrontPage Parallax Section', 'shapely-companion' ),
 			'customize_selective_refresh' => true,
 		);
-		parent::__construct( 'shapely_home_parallax', esc_html__( '[Shapely] Parralax Section For FrontPage', 'shapely-companion' ), $widget_ops );
+		parent::__construct( 'shapely_home_parallax', esc_html__( '[Shapely] Parallax Section For FrontPage', 'shapely-companion' ), $widget_ops );
 
 		$this->defaults = array(
 			'title'         => '',
@@ -106,8 +106,8 @@ class Shapely_Home_Parallax extends WP_Widget {
 										echo apply_filters( 'the_content', wp_kses( $instance['body_content'], $allowed_tags ) );
 										echo '</div>';
 									}
-									echo ( '' != $instance['button2'] && '' != $instance['button2_link'] ) ? '<a class="btn btn-lg btn-white" href="' . esc_url( $instance['button2_link'] ) . '">' . wp_kses_post( $instance['button2'] ) . '</a>' : '';
 									echo ( '' != $instance['button1'] && '' != $instance['button1_link'] ) ? '<a class="btn btn-lg btn-filled" href="' . esc_url( $instance['button1_link'] ) . '">' . wp_kses_post( $instance['button1'] ) . '</a>' : '';
+									echo ( '' != $instance['button2'] && '' != $instance['button2_link'] ) ? '<a class="btn btn-lg btn-white" href="' . esc_url( $instance['button2_link'] ) . '">' . wp_kses_post( $instance['button2'] ) . '</a>' : '';
 									?>
 								</div>
 							</div>
